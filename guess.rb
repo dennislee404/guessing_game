a30928e78b53cba5c553a68f80389cdf0c41280b
Dennis Lee <dennislee404@gmail.com>
2023-08-17 09:08:37 +0800

    Added difficulty

--- guess.rb
+++ guess.rb
@@ -1,67 +1,99 @@
-scoring_card = {}
+require 'colorize'
 
-#restart = "y"
-#while restart == "y"
-loop do
+@leaderboard = {}
 
-	puts "Please enter your name"
-	player_name = gets.chomp
-	puts "Hello #{player_name}"
-
-	secret_number = rand(100)
-	puts secret_number
-
-	counter = 1
-	min = 1
-	max = 100
-	guesses = []
-	while counter <= 6
-
-		puts "Try ##{counter}. Please guess a number between #{min}-#{max}"
-		answer = gets.chomp.to_i
-
-		if answer > secret_number
-			puts "Number is too high"
-			max = answer
-		elsif answer < secret_number
-			puts "Number is too low"
-			min = answer
-		else
-			break puts "Congratulations! Number is correct"
-		end
-			
-		guesses << answer
-		puts "guesses: #{guesses}"
-		counter += 1
-
-	end
-
-	puts "The answer is #{secret_number}"
-
-	puts "HIGHSCORE"
-	if scoring_card[player_name] == nil
-		scoring_card[player_name] = counter
+def difficulty()
+	puts "Please select the difficulty (Easy, Medium, Hard)".red
+	@difficulty = gets.chomp.downcase
+	if @difficulty == "easy"
+		@cap = 20
+	elsif @difficulty == "medium"
+		@cap = 50
 	else
-		if scoring_card[player_name] > counter
-			scoring_card[player_name] = counter
-		end
+		@cap = 100
 	end
-	#puts scoring_card
-	#puts scoring_card[player_name]
-	#scoring_card[player_name] = i
-	#puts scoring_card['Dennis']
-	scoring_card.sort_by {|player,score| score}.each do |player, score|
-	#scoring_card.sort_by(&:last).each do |player, score|
-		puts "#{player}: #{score}"
-	end
-
-	puts "Enter 'Y' if you wish to play again"
-	restart = gets.chomp.downcase
-	break if restart != "y"
-	
 end
 
-puts "Thank you for playing"
+def set_default(cap)
+	@secret_number = rand(1..@cap)
+	#puts @secret_number
+	@counter = 1
+	@min = 1
+	@max = @cap
+	@guesses = []
+end
+
+def get_name()
+	puts "Enter your name to start a new game or type 'X' to exit the game".red
+	@player_name = gets.chomp.upcase
+end
+
+def guess()
+	while @counter <= 6
+
+		puts "Try ##{@counter}. Please guess a number between #{@min}-#{@max}".red
+		answer = gets.chomp.to_i
+
+		if answer > @secret_number
+			puts "Number is too high".red
+			@max = answer 
+		elsif answer < @secret_number
+			puts "Number is too low".red
+			@min = answer
+		else
+			break puts "Congratulations! Number is correct".green
+		end
+			
+		@guesses << answer
+		puts "guesses: #{@guesses}"
+		@counter += 1
+	end	
+	puts "The answer is #{@secret_number}".red
+end
+
+def update_leaderboard()
+	if @leaderboard[@player_name] == nil
+		@leaderboard[@player_name] = @counter
+	else
+		if @leaderboard[@player_name] > @counter
+			@leaderboard[@player_name] = @counter
+		end
+	end
+end
+
+def print_leaderboard()
+	puts "HIGHSCORE".blue
+	@leaderboard.sort_by(&:last).each do |player, score|
+		puts "#{player}: #{score}".blue
+	end
+end
+
+def restart_game()
+	puts "Enter 'Y' to play again".red
+	@restart = gets.chomp.downcase
+end
+
+def run_game()
+	loop do
+		get_name()
+		break if @player_name == "X"
+		puts "Hello #{@player_name}".red
+		loop do
+			difficulty()
+			set_default(@num)
+			guess()
+			update_leaderboard()	
+			print_leaderboard()
+			restart_game()
+			break if @restart != "y"
+		end
+	end
+	puts "Thank you for playing".red
+end
+
+run_game()
+
+
 
 
 
